479d60878d623e13cc95038c97d78539881f6e5e
Enedi Godswill <bobbyenedi@gmail.com>
2024-07-03 14:13:45 +0100

    second commit

--- js/App.js
+++ js/App.js
@@ -19,20 +19,45 @@ themeToggle.addEventListener("click", () => {
 });
 
 
-// Time and day
-const days = document.getElementById("day");
 
-function date(){
-    const d = new Date();
-    let day = d.getDay();
+// Time
 
+const hourEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function clock(){
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h > 12){
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minutesEl.innerText = m;
+    secondsEl.innerText = s;
+    ampm, (innerText = ampm);
     setTimeout(()=> {
-        date()
-    }, 1000)
-
-    days.innerText = d;
+        clock();
+    }, 1000);
 }
 
-date();
+clock();
 
+// current Day
+
+const weekdayEl = document.getElementById("weekday"); //Weekday element
+const weekday = ["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; //weekdays
+const d = new Date();
+let day = weekday[d.getDay()];
+weekdayEl.innerText = day;
 
